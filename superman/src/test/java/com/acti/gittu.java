08ea7769c4145192b9c6c04643835bd4696bd98a
admin <admin@DESKTOP-O8TQ429>
2024-06-12 19:52:17 +0530

    MVN project

---
+++ superman/src/test/java/com/acti/gittu.java
@@ -0,0 +1,13 @@
+package com.acti;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class gittu {
+	@Test
+	public void demo() {
+		Reporter.log("hi world");
+	}
+	
+
+}
